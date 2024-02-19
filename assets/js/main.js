a390d4d1adc0e572f0fce8c5f6b39841fff310e5
mandajega <amanda.jeganathan@gmail.com>
2024-02-19 23:08:06 +0530

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,13 +28,13 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
+const bgHeader = () =>{
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', bgHeader)
 
 /*=============== TESTIMONIAL SWIPER ===============*/
 let testimonialSwiper = new Swiper(".testimonial-swiper", {
