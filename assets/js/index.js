25772f07c6523fced22cd95631cdf029667a387a
mandajega <amanda.jeganathan@gmail.com>
2024-02-27 11:35:47 +0530

    main

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,7 +22,7 @@ modalClose.forEach((modalClose) => {
      
 })
 
-const swiper = new Swiper('.testimonial__swiper', {
+const swiperTestimonial = new Swiper('.testimonial__swiper', {
     // Optional parameters
     
     loop: true,
@@ -34,4 +34,13 @@ const swiper = new Swiper('.testimonial__swiper', {
       clickable: true,
     },
   
-  });
+  })
+
+  /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
